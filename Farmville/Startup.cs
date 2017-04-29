5cb3536eda7b9219c94122bdf45f77a6d353e933
Blake Denniston <bd21@uw.edu>
2017-04-29 11:43:08 -0700

    Add project files.

---
+++ Farmville/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Farmville.Startup))]
+namespace Farmville
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
