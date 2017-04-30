e1b38645b85e6b5175a8456b7200439147d0ba5d
Blake Denniston <bd21@uw.edu>
2017-04-29 19:57:58 -0700

    progress farmer page workiing

---
+++ Farmville/Models/FarmerSideModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Farmville.Models
+{
+    enum Crop { Corn, Oats, Barley, Rice, Tobacco, Wheat, Soybean};
+    enum Cattle { Beef, Chicken, Pork, Lamb, Milk }
+    public class FarmerSideModel
+    {
+        [Key]
+        int Id { get; set; }
+        Crop cropType { get; set; }
+        Cattle cattleType { get; set; }
+        int cropQuantity { get; set; }
+        int acres { get; set; }
+        String measurementType { get; set; }
+        int zipcode { get; set; }
+        //API TO GET CURRENT PRICES?
+    }
+
+    public class FarmerInfoDb : DbContext
+    {
+        public DbSet<FarmerInfoDb> Movies { get; set; }
+    }
+}
